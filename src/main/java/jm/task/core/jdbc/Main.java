4bb3a66931434817b1de6526a17d3619f27973fe
Igor Anikeev <anikeev@softlab.ru>
2024-01-24 16:19:26 +0400

    Initial commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
         userService.createUsersTable();
-        userService.saveUser("Петя", "Один", (byte) 78);
-        userService.saveUser("Вася", "Двойко", (byte) 74);
-        userService.saveUser("Света", "Трояк", (byte) 59);
-        userService.saveUser("Катя", "Четверышка", (byte) 74);
+        userService.saveUser("1", "1", (byte) 1);
+        userService.saveUser("2", "2", (byte) 2);
+        userService.saveUser("3", "3", (byte) 3);
+        userService.saveUser("4", "4", (byte) 4);
         userService.removeUserById(2);
         userService.getAllUsers();
         userService.cleanUsersTable();
